8b42d3dfc4289be3a73ba4ddf99836417f1f7122
TheBicho7 <105554569+TheBicho7@users.noreply.github.com>
2022-09-24 16:02:06 -0500

    primera preEntrega Proyecto Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import CartWidget from "./components/NavBar/CartWidget";
+import  FontAwesomeIcon  from "@fortawesome/react-fontawesome";
 
+// Componente
 function App() {
+  let misEstilos = { backgroundColor: "lightblue" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={misEstilos}>
+      <CartWidget />
+      <ItemListContainer greeting="Bienvenidos a nuestra plataforma de cursos" />
     </div>
   );
 }
---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserNinja } from "@fortawesome/free-solid-svg-icons";
+
+function CartWidget() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faUserNinja} />
+    </div>
+  );
+}
+
+export default CartWidget;
